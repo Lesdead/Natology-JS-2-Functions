571d5214b89fd3890f456a8fb756b1be0809ade7
lesdead <lesdead@gmail.com>
2022-12-06 21:34:14 +0200

    all files added

---
+++ task.js
@@ -0,0 +1,50 @@
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
+
+  for (let i = 0; i <  arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i]; 
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return { min:min, max:max, avg:avg };
+}
+
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+
+  arr.forEach(item => sum += item);
+
+  return sum;
+}
+
+function makeWork(arrOfArr, func) {
+  let max = -Infinity;
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) max = sum;
+  }
+
+  return max;
+}
+
+// Задание 3
+function worker2(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i <  arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+  }
+
+  return max - min;
+}
